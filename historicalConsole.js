39fdd0a62780d76cb09a668eb6867f1e58062542
Devin Rhode <devinrhode2@gmail.com>
2013-02-28 16:59:34 -0800

    Move methods common to all `console` objects to..
    parent `historicalConsole` object. This results in less memory usage so that every console object doesn't contain the method, there's only one instance on historicalConsole

--- historicalConsole.js
+++ historicalConsole.js
@@ -30,6 +30,127 @@ if (typeof window !== 'undefined' && window.ie == null) {
 
   var nativeConsole = window.console;
 
+  function historicalConsole(fn) {
+  
+    //validate callback:
+    if (Object.prototype.toString.call(fn) != '[object Function]' ||
+        arguments.length !== 1 ||
+        fn.length !== 1)
+    {
+      var message = 'historicalConsole expects one function argument like this: ' +
+                    'historicalConsole(function(console){/*your whole program*/});';
+      alert(message);
+      console.error(message + ' You passed in these arguments: ', arguments);
+      return;
+    }
+
+    //returning a function allows for more flexible use of `historicalConsole`
+    return function historicalConsoleClosure() {
+      //override global console temporarily so that console.* calls from externals
+      //are logged too. This could be developer feedback from external libraries
+      //regarding the callbacks use of that library
+      var oldConsole = window.console;
+      window.console = console;
+      try {
+        fn(console);
+      } catch (e) {
+        if (typeof onuncaughtException !== 'undefined') { //probably window.onuncaughtError but maybe not. you can var over it
+          onuncaughtException(e);
+        } else {
+          this.console && console.warn && console.warn(
+            'You should define a window.onuncaughtException handler for exceptions,' +
+            ' or use a library like Sheild.js'
+          );
+          throw e;
+        }
+      } finally { //finally block so we can restore window.console even if we re-throw an error
+        window.console = oldConsole;
+      }
+    };
+  }
+
+  //add this to parent historicalConsole since it's common among every instantiated console
+  //a 'class' method. prototypes could be used,
+  //but I don't think we could do that and have a big clean var console = {...}, which is fast for interpreters
+  historicalConsole.resolveFunctionName: function historicalConsole_resolveFunctionName(func) {
+    if (!func) {
+      return 'null'; //null is when there is not caller (global scope)
+    }
+    var toString = func.toString()
+                    .substring(0, 250) //so the regex doesn't become a performance hog
+                    .replace(/\s+/g, ' ');
+    //collapse excess whitespace so function snippits for
+    //non-named functions are more much more informative
+
+    if (window.ie) {
+      func.name = toString
+                     //all chars be 'function' and '(' (if there are no chars, then '')
+                    .substring(8, toString.indexOf('(', 8)) 
+                    .replace(/^\s+|\s+$/g,''); //'function (){' => ' ' becomes the falsey ''
+    }
+    return func.name || toString.substring(0, internalOptions.functionSnippetLength);
+  };
+
+  // Big thanks to @NV for console.js: github.com/NV/console.js
+  // saveHooks modify arguments before being saved to console.history
+  // the return values are the modified args
+  //historicalConsole.saveHooks
+  historicalConsole.saveHooks = {
+    assert: function console_saveHook_assert(isOk, message) {
+      return ['Assertion ' + (isOk ? 'successful' : 'failed') + ': ' + message];
+    },
+    count: (function() {
+      var counters = {};
+      return function console_saveHook_count(title) {
+        if (!title) {
+          //this is the *key* to counters, not the *value*
+          title = Math.floor((Math.random() * 100000) + 1).toString();
+        }
+        if (counters[title]) {
+          counters[title]++;
+        } else {
+          counters[title] = 1;
+        }
+        return title + ' ' + counters[title];
+      };
+    })(),
+    /*
+    //stringifyArguments/_source_of(argument)
+    //https://github.com/eriwen/javascript-stacktrace/blob/master/stacktrace.js#L272-L300
+    //https://github.com/NV/console.js/blob/gh-pages/console.js#L70-L131
+    dir: function console_saveHook_dir() {
+      var result = [];
+      for (var i = 0; i < arguments.length; i++) {
+        result.push(console._source_of(arguments[i], console.dimensions_limit, []));
+      }
+      return result.join(console._args_separator);
+    },
+    */
+    /*
+    //try to glean something from jsperf:
+    profile: function console_saveHook_profile() {
+    },
+    profileEnd: function console_saveHook_profileEnd() {
+    },
+    */
+    time: function console_saveHook_time(name) {
+      if (name === undefined) {
+        throw new Error('console.time needs a title for your timing like console.time(\'lookup\')');
+      }
+      startTimes[name] = (new Date()).getTime();
+      return [name];
+    },
+    timeEnd: function console_saveHook_timeEnd(name) {
+      return [(name + ': ' + ((new Date()).getTime() - startTimes[name]) + 'ms')];
+    },
+    timeStamp: function console_saveHook_timeStamp(optionalLabel) {
+      return [(new Date()).getTime(), optionalLabel];
+    },
+    trace: function console_saveHook_trace() {
+      return [/*counter?*/ (new Error('console.trace()')).stack || 'stack traces not supported'];
+    }
+  };
+
   //declare as many console properties and methods upfront for optimization
   var console = {
     /**
@@ -62,7 +183,7 @@ if (typeof window !== 'undefined' && window.ie == null) {
         if (internalOptions.addCaller) {
           try {
             //try to addCaller - separate from above because it may not succeed
-            args.push('caller:' + console.resolveFunctionName(arguments.callee.caller));
+            args.push('caller:' + historicalConsole.resolveFunctionName(arguments.callee.caller));
           } catch (e) {
             console.warn(
               'SOMEBODY made a global \'use strict\'; statement. ' +
@@ -74,96 +195,12 @@ if (typeof window !== 'undefined' && window.ie == null) {
         console.history.add(args);
       };
     },
-    
-    //historicalConsole.resolveFunctionName
-    resolveFunctionName: function console_resolveFunctionName(func) {
-      if (!func) {
-        return 'null'; //null is when there is not caller (global scope)
-      }
-      var toString = func.toString()
-                      .substring(0, 250) //so the regex doesn't become a performance hog
-                      .replace(/\s+/g, ' ');
-      //collapse excess whitespace so function snippits for
-      //non-named functions are more much more informative
-  
-      if (window.ie) {
-        func.name = toString
-                       //all chars be 'function' and '(' (if there are no chars, then '')
-                      .substring(8, toString.indexOf('(', 8)) 
-                      .replace(/^\s+|\s+$/g,''); //'function (){' => ' ' becomes the falsey ''
-      }
-      return func.name || toString.substring(0, internalOptions.functionSnippetLength);
-    },
 
     //this.history
     history: [],
     //we don't do console.history = console.history || []
     //because we don't want to inherit other history from another party
-
-    // ****************************
-    // ** DEFINE CONSOLE METHODS **
-    // ****************************
-    // Big thanks to @NV for console.js: github.com/NV/console.js
-  
-    // saveHooks modify arguments before being saved to console.history
-    // the return values are the modified args
-    //historicalConsole.saveHooks
-    saveHooks: {
-      assert: function console_saveHook_assert(isOk, message) {
-        return ['Assertion ' + (isOk ? 'successful' : 'failed') + ': ' + message];
-      },
-      count: (function() {
-        var counters = {};
-        return function console_saveHook_count(title) {
-          if (!title) {
-            //this is the *key* to counters, not the *value*
-            title = Math.floor((Math.random() * 100000) + 1).toString();
-          }
-          if (counters[title]) {
-            counters[title]++;
-          } else {
-            counters[title] = 1;
-          }
-          return title + ' ' + counters[title];
-        };
-      })(),
-      /*
-      //stringifyArguments/_source_of(argument)
-      //https://github.com/eriwen/javascript-stacktrace/blob/master/stacktrace.js#L272-L300
-      //https://github.com/NV/console.js/blob/gh-pages/console.js#L70-L131
-      dir: function console_saveHook_dir() {
-        var result = [];
-        for (var i = 0; i < arguments.length; i++) {
-          result.push(console._source_of(arguments[i], console.dimensions_limit, []));
-        }
-        return result.join(console._args_separator);
-      },
-      */
-      /*
-      //try to glean something from jsperf:
-      profile: function console_saveHook_profile() {
-      },
-      profileEnd: function console_saveHook_profileEnd() {
-      },
-      */
-      time: function console_saveHook_time(name) {
-        if (name === undefined) {
-          throw new Error('console.time needs a title for your timing like console.time(\'lookup\')');
-        }
-        startTimes[name] = (new Date()).getTime();
-        return [name];
-      },
-      timeEnd: function console_saveHook_timeEnd(name) {
-        return [(name + ': ' + ((new Date()).getTime() - startTimes[name]) + 'ms')];
-      },
-      timeStamp: function console_saveHook_timeStamp(optionalLabel) {
-        return [(new Date()).getTime(), optionalLabel];
-      },
-      trace: function console_saveHook_trace() {
-        return [/*counter?*/ (new Error('console.trace()')).stack || 'stack traces not supported'];
-      }
-    },
-    
+ 
     //depends on: console.warn
     //this.generateOptionFunction
     generateOptionFunction: function console_generateOptionFunction(optionKey) {
@@ -215,7 +252,7 @@ if (typeof window !== 'undefined' && window.ie == null) {
     console[method] = console.generateConsoleMethod(
       method,
       nativeConsole[method] || nativeConsole.log,
-      console.saveHooks[method]
+      historicalConsole.saveHooks[method]
     );
     //if no console method, default to console.log. Thanks to @cowboy:
     //http://benalman.com/projects/javascript-debug-console-log/
@@ -245,44 +282,6 @@ if (typeof window !== 'undefined' && window.ie == null) {
     }
   }
 
-  function historicalConsole(fn) {
-  
-    //validate callback:
-    if (Object.prototype.toString.call(fn) != '[object Function]' ||
-        arguments.length !== 1 ||
-        fn.length !== 1)
-    {
-      var message = 'historicalConsole expects one function argument like this: ' +
-                    'historicalConsole(function(console){/*your whole program*/});';
-      alert(message);
-      console.error(message + ' You passed in these arguments: ', arguments);
-      return;
-    }
-
-    //returning a function allows for more flexible use of `historicalConsole`
-    return function historicalConsoleClosure() {
-      //override global console temporarily so that console.* calls from externals
-      //are logged too. This could be developer feedback from external libraries
-      //regarding the callbacks use of that library
-      var oldConsole = window.console;
-      window.console = console;
-      try {
-        fn(console);
-      } catch (e) {
-        if (typeof onuncaughtException !== 'undefined') { //probably window.onuncaughtError but maybe not. you can var over it
-          onuncaughtException(e);
-        } else {
-          this.console && console.warn && console.warn(
-            'You should define a window.onuncaughtException handler for exceptions,' +
-            ' or use a library like Sheild.js'
-          );
-          throw e;
-        }
-      } finally { //finally block so we can restore window.console even if we re-throw an error
-        window.console = oldConsole;
-      }
-    };
-  }
 
   historicalConsole.noConflict = function historicalConsole_noConflict() {
     window.historicalConsole = _oldHistoricalConsole;
